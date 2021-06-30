8af5ccc7763d30a39106b1159c9acc07b58fd053
sacchit25 <sacchitmittal@gmail.com>
2021-07-01 02:11:35 +0300

    commit

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1></h1>
+            <p>
+                <mark class="white">RateMy</mark>
+                <mark class="yellow">Internship</mark>
+            </p>
+            <h3> Write a review </h3>
+            <img 
+        src="https://rscbga.com/wp-content/uploads/2021/04/png-internship-8-png-image-internship-png-1599_533.png" 
+        alt="Problem?"
+    />
+        </header>
+    )
+}
+
+export default Header;
