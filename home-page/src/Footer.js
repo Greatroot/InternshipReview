b97bcb4e04bbe24d710b850737fe225835f20028
sacchit25 <sacchitmittal@gmail.com>
2021-07-01 11:32:29 +0300

    commit2

---
+++ home-page/src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Footer() {
+    return (
+    <div>
+        <footer>
+            <h1> Follow Us </h1>
+            <li> <a href = "#"> < i class="fa fa-linkedin"></i> </a></li>
+            <li> <a href = "#"> < i class="fa fa-instagram"></i> </a></li>
+        </footer>
+    </div>
+    )
+}
+export default Footer;
