73176e333bc36104f87c2e7299caca9dde6883dc
Ben Kosa <bkosa2@cs.washington.edu>
2021-07-02 12:23:21 -0700

    Modified the new header with my own version and added CSS for the header through Header.css. There is something terribly wrong with our file structure though, as there are two different versions currently in one. I'm going to push this version first, and then attempt to fix it.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import Header from './Header/Header';
 import Home from "./Home"
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// TODO: Update the social media links once we make actual accounts.
 function App() {
     return (
-        <div>
+        <BrowserRouter>
             <Header />
-            <Home />
-        </div>
+            <Switch>
+                <Route path='/' exact component={Home} />
+                <Route path='/instagram' component={() => {
+                     window.location.href = 'https://www.instagram.com';
+                     return null;
+                }} />
+                <Route path='/instagram' component={() => {
+                    window.location.href = 'https://www.instagram.com';
+                    return null;
+                }} />
+                <Route path='/linked-in' component={() => {
+                    window.location.href = 'https://www.linkedin.com/in/ben-kosa-2b04251ab/';
+                    return null;
+                }} />
+            </Switch>
+        </BrowserRouter>
     )
 }
 
