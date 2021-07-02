188c138ec176595e49b32f75eb697cfac230061f
sacchit25 <sacchitmittal@gmail.com>
2021-07-02 22:59:47 +0300

    added some styling

--- home-page/src/BlackComponent.js
+++ home-page/src/BlackComponent.js
@@ -6,9 +6,12 @@ function BlackComponent() {
         <div>
             <div className="black-comp">
             <br /> <br /> <br /> <br />  <br /> <br />
-            <p className="text">Ever wished to intern at top companies? <br />
+            <p className="text">
+            <mark class="white">Ever wished to intern at top companies? <br />
             Reviews by students for students.<br />
-            A journey towards a meaningful career.<br />
+            A journey towards a </mark>  
+            <mark class="red">meaningful </mark> 
+            career.<br />
              </p>
              <br /> <br /> <br /> <br />  <br /> <br /> 
         </div>
--- home-page/src/WhiteComponent.js
+++ home-page/src/WhiteComponent.js
@@ -4,9 +4,12 @@ function WhiteComponent() {
     return(
         <div className="comp">
             <br /> <br /> <br /> <br />  <br /> <br />
-            <p className="text">Applying as well as finalizing on an<br />
+            <p className="text">
+            <mark class="black">Applying as well as finalizing on an<br />
             internship offer is daunting. We are here to<br />
-            make interning fun by helping you find<br />
+            make </mark>
+            <mark class="red">interning </mark> 
+            fun by helping you find<br />
             the perfect internship. </p>
             <br /> <br /> <br /> <br />  <br /> <br />
         </div>
