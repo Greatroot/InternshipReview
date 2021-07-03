b6b217ec8f3a834de7c9435cc1c0dfddd5f1f7fb
Ben Kosa <bkosa2@cs.washington.edu>
2021-07-02 18:36:54 -0700

    I cloned Sacchit's version of main and we both manually merged both my version and his version of the web app so far to create this commit. I also split the entire repo into two files (Frontend and Backend) and put our React web app into the Frontend folder.

---
+++ Frontend/home-page/src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+        <header>
+            <p className="rmi">
+                <mark class="white">RateMy</mark>
+                <mark class="yellow">Internship</mark>
+            </p>
+            <h3> Write a review </h3>
+        </header>
+        </div>
+    )
+}
+
+export default Header;
---
+++ Frontend/home-page/src/Image.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Image() {
+    return (
+     <div>
+        <img 
+        src="https://rscbga.com/wp-content/uploads/2021/04/png-internship-8-png-image-internship-png-1599_533.png" 
+        alt="Problem?"
+    />
+    <br />  <br /> 
+    </div> 
+    )
+
+}
+
+export default Image
