94c7acc068188dfb9bedf9c79bf0c2ee708c16cd
Ben Kosa <b.kkosa2@gmail.com>
2021-07-10 08:03:08 -0700

    This is temporary and doesn't fix the issue I had with merging.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-<<<<<<< HEAD:Frontend/src/App.js
 import Header from './components/Header/Header';
 import Home from "./pages/Home/Home";
 import Results from "./pages/Results/Results";
 import Footer from "./pages/Home/Footer";
-=======
-import Header from './Header/Header';
-import Home from "./Home/Home";
-import DesignComponent from './Home/DesignComponent';
-import Footer from "./Home/Footer/Footer.js";
->>>>>>> 8a26c7db0ed951d6a1441dcd51341b9ba6681bfe:Frontend/home-page/src/App.js
 
 // TODO: Update the social media links once we make actual accounts.
 function App() {
