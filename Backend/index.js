8a2020bae2ef28374862afbfb000ead4aefc7fe1
arumilla <jennifercao291953@gmail.com>
2021-07-12 15:14:18 -0700

    Redoing database - WIP
    Still need to change names and stuff

--- Backend/index.js
+++ Backend/index.js
@@ -7,16 +7,15 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    user: 'root',
-    host: 'localhost',
-    password: 'password',
-    database: 'internship_review',
+    user: 'bcb45b2cf00546',
+    host: 'us-cdbr-east-04.cleardb.com',
+    password: '1cf42285',
+    database: 'heroku_3c7763526e153a6',
 });
 
 app.put('/update', (req, res) => {
     const id = req.body.id
     const comments = req.body.comments
-
 
     db.query(
         "UPDATE reviews SET comments = ? WHERE id = ?",
