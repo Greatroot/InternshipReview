ce4b910d87f0dfb76be9c45203acf8b02b67e0b6
seasonphia <sxw2@uw.edu>
2021-07-26 20:44:34 -0700

    home page dynamic and mostly done

--- Frontend/src/pages/Form/Search.js
+++ Frontend/src/pages/Form/Search.js
@@ -111,18 +111,18 @@ function Search() {
                         }}
                     >
                         <option value="">Month</option>
-                        <option value="January"> January</option>
-                        <option value="February">February </option>
-                        <option value="March">March </option>
-                        <option value="April">April </option>
-                        <option value="May"> May</option>
-                        <option value="June"> June</option>
-                        <option value="July"> July</option>
-                        <option value="August"> August</option>
-                        <option value="September"> September</option>
-                        <option value="October"> October</option>
-                        <option value="November"> November</option>
-                        <option value="December">December </option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
                     </select>
                 </div>
                 <div className="start-year">
@@ -134,12 +134,12 @@ function Search() {
                         }}
                     >
                         <option value="">Year</option>
-                        <option value="2016"> 2016</option>
-                        <option value="2017">2017 </option>
-                        <option value="2018">2018 </option>
-                        <option value="2019">2019 </option>
-                        <option value="2020"> 2020</option>
-                        <option value="2021"> 2021</option>
+                        <option value="2016">2016</option>
+                        <option value="2017">2017</option>
+                        <option value="2018">2018</option>
+                        <option value="2019">2019</option>
+                        <option value="2020">2020</option>
+                        <option value="2021">2021</option>
                     </select>
                 </div>
                 <br /><br /><br />
